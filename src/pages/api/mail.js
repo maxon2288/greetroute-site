ad71e9020a4c9b4659d96a2e2b41281e04a00d35
maxon2288 <maxswim228@gmail.com>
2023-12-01 22:23:23 +0100

    docker update 1111444

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -24,39 +24,19 @@ const mail = require('@sendgrid/mail')
 export default async (req, res) => {
 	mail.setApiKey('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')
 	console.log(req.body)
-	const body = JSON.parse(req.body)
-	const message = `
+	const body = req.body
+	const message = ` 
 	<table width='100%' cellpadding='0' cellspacing='0'>
         <tr>
             <td>
                 <table style='width: 100%'>
                     <tr>
                         <td>
-                            <h1>Новая заявка с сайта syncup.kz</h1>
+                            <h1>Новая заявка с сайта GreetRoute</h1>
                             <table style='max-width: 600px; width: 100%'>
-                                <tr>
+                            <tr>
                                     <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
-                                        Имя
-                                        
-                                    </td>
-                                    <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
-                                        ${body.name}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
-                                        Почта/телефон
-                                    </td>
-                                    <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
-                                        ${body.email}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
-                                        Сообщение
-                                    </td>
-                                    <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
-                                        ${body.message}
+                                        ${req.body}
                                     </td>
                                 </tr>
                             </table>
@@ -78,6 +58,32 @@ export default async (req, res) => {
 		res.status(200).json({ status: 'Ok' })
 	})
 }
+
+// <tr>
+//     <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
+//         Имя
+//
+//     </td>
+//     <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
+//         ${body.name}
+//     </td>
+// </tr>
+// <tr>
+//     <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
+//         Почта/телефон
+//     </td>
+//     <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
+//         ${body.email}
+//     </td>
+// </tr>
+// <tr>
+//     <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
+//         Сообщение
+//     </td>
+//     <td style='border-bottom: 1px solid #cccccc; padding: 16px 0;'>
+//         ${body.message}
+//     </td>
+// </tr>
 
 // import nodemailer from 'nodemailer'
 //
