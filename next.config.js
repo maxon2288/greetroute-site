b5cfac1d32c5c95ac56eb6957a36f5578e79020a
maxon2288 <maxswim228@gmail.com>
2023-12-02 15:17:12 +0100

    docker update 1111444

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		NEXT_PUBLIC_MAIL_SENDGRID_PASS: "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"
+	}
+}
 
 module.exports = nextConfig
